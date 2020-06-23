09894d8306cbf017c864d0d95649be48b60a7957
Silkypaladin <bednarski.szymon99@gmail.com>
2020-06-23 16:35:08 +0200

    Prepare models for Quiz and Question

--- api/models.py
+++ api/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     nickname = db.Column(db.String(100), unique=True)
     is_admin = db.Column(db.Boolean(), default=False)
     date_created = db.Column(db.DateTime, default=db.func.now())
+    quizzes = db.relationship('Quiz', backref='user', lazy=True)
 
 
     def __init__(self, public_id, email, password, nickname):
@@ -17,3 +18,35 @@ class User(db.Model):
       self.email = email
       self.password = password
       self.nickname = nickname
+
+
+class Quiz(db.Model):
+  __tablename__ = "quizzes"
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(50))
+  info = db.Column(db.String(200))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  questions = db.relationship('Question', backref='quiz', lazy=True)
+
+  def __init__(self, title, info, user_id):
+    self.title = title
+    self.info = info
+    self.user_id = user_id
+
+
+class Question(db.Model):
+   __tablename__ = "questions"
+
+   id = db.Column(db.Integer, primary_key=True)
+   question = db.Column(db.String(200), unique=True)
+   ans_one = db.Column(db.String(200), nullable=False)
+   ans_two = db.Column(db.String(200), nullable=False)
+   ans_three = db.Column(db.String(200))
+   ans_four = db.Column(db.String(200))
+   correct = db.Column(db.Integer,nullable=False)
+   quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'), nullable=False)
+   
+   
+   
+
